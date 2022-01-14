3fe2b17446c1b1ba4d1e39111a3753540092608f
bwill619 <bjw0619@gmail.com>
2022-01-13 20:39:57 -0500

    commit on main

--- api.py
+++ api.py
@@ -1,10 +1,8 @@
 import requests
 
-city = 'charlotte'
+url = "https://community-open-weather-map.p.rapidapi.com/weather"
 
-url = "https://community-open-weather-map.p.rapidapi.com/find"
-
-querystring = {"q":"london","cnt":"0","mode":"null","lon":"0","type":"link, accurate","lat":"0","units":"imperial, metric"}
+querystring = {"q":"London,uk","lat":"0","lon":"0","callback":"test","id":"2172797","lang":"null","units":"imperial","mode":"xml"}
 
 headers = {
     'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
